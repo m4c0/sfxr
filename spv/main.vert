ce4cc953b6c5a165b61b1b08ce11c3c8f33909d1
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-11 19:58:39 +0100

    magic pixels using vulkan instanced stuff

--- spv/main.vert
+++ spv/main.vert
@@ -7,11 +7,13 @@ layout(push_constant) uniform upc {
 } m;
 
 layout(location = 0) in vec2 vertex;
+layout(location = 1) in ivec2 instance;
+layout(location = 2) in uvec4 color;
 
 layout(location = 0) out vec4 frag_color;
 
 void main() {
-  gl_Position = vec4(vertex / m.pixels, 0, 1);
-  frag_color = vec4(0, 0, 1, 1);
+  gl_Position = vec4((vertex + instance) / m.pixels, 0, 1);
+  frag_color = color / 256.0;
 }
 
--- src/m4c0.vulkan.hpp
+++ src/m4c0.vulkan.hpp
@@ -74,18 +74,42 @@ class color_mem {
   using memory = m4c0::vulkan::device_memory;
   using bind = m4c0::vulkan::tools::buffer_memory_bind;
 
+  unsigned m_count;
   buffer m_buffer;
   memory m_memory;
   bind m_bind;
 
+  struct instance {
+    alignas(2) std::int16_t x, y;
+    std::uint32_t rgba;
+  };
+  // static_assert(sizeof(instance) == sizeof(std::uint64_t));
+
 public:
   color_mem(const m4c0::fuji::device_context * ld, std::uint16_t w, std::uint16_t h)
-    : m_buffer(buffer::create_vertex_buffer_with_size(w * h * 4))
+    : m_count(w * h)
+    , m_buffer(buffer::create_vertex_buffer_with_size(m_count * sizeof(instance)))
     , m_memory(ld->create_host_memory(&m_buffer))
     , m_bind(&m_buffer, &m_memory) {
+    auto guard = m_memory.map_all();
+    auto * ptr = guard.pointer<instance>();
+    for (int x = 0; x < w; x++) {
+      for (int y = 0; y < h; y++) {
+        *ptr++ = instance { static_cast<int16_t>(x - w / 2),
+                            static_cast<int16_t>(y - h / 2),
+                            static_cast<uint32_t>((x << 16) | (y << 8)) }; // NOLINT
+      }
+    }
   }
   void build_secondary_command_buffer(VkCommandBuffer cb) {
     m4c0::vulkan::cmd::bind_vertex_buffer(cb).with_buffer(&m_buffer).with_first_index(1).now();
+  }
+
+  [[nodiscard]] constexpr auto count() const noexcept {
+    return m_count;
+  }
+  [[nodiscard]] static constexpr auto stride() noexcept {
+    return sizeof(instance);
   }
 };
 
@@ -108,11 +132,14 @@ public:
                      .with_pipeline_layout(&m_pipeline_layout)
                      .with_render_pass(ld->render_pass())
                      .add_vertex_binding_with_stride(vtx_mem::stride())
+                     .add_vertex_binding_instanced_with_stride(color_mem::stride())
                      .add_vec2_attribute_with_bind_and_offset(0, 0)
+                     .add_s16vec2_attribute_with_bind_and_offset(1, 0)
+                     .add_u8vec4_attribute_with_bind_and_offset(1, 4)
                      .add_vertex_stage(shader(main_vert_spv, main_vert_spv_len), "main")
                      .add_fragment_stage(shader(main_frag_spv, main_frag_spv_len), "main")
                      .build())
-    , m_consts({ static_cast<float>(w), static_cast<float>(h) }) {
+    , m_consts({ static_cast<float>(w) / 2, static_cast<float>(h) / 2 }) {
   }
 
   void build_secondary_command_buffer(VkCommandBuffer cb) {
@@ -143,8 +170,8 @@ public:
     m_viewport.build_command_buffer(cb);
     m_pipeline.build_secondary_command_buffer(cb);
     m_vtx_mem.build_secondary_command_buffer(cb);
-    // m_clr_mem.build_secondary_command_buffer(cb);
-    m4c0::vulkan::cmd::draw(cb).with_vertex_count(vtx_mem::count()).now();
+    m_clr_mem.build_secondary_command_buffer(cb);
+    m4c0::vulkan::cmd::draw(cb).with_vertex_count(vtx_mem::count()).with_instance_count(m_clr_mem.count()).now();
   }
   void on_render_extent_change(m4c0::vulkan::extent_2d e) override {
     m_viewport.extent() = e;
