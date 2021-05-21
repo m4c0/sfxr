cf530405ff09f54fd9705c23d2828e513abad0a5
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-21 08:35:39 +0100

    updating to avoid direct usage of ObjC

--- src/m4c0.osx.cpp
+++ src/m4c0.osx.cpp
@@ -1,23 +1,18 @@
 #include "m4c0.vulkan.hpp"
 #include "m4c0/fuji/main_loop_thread.hpp"
 #include "m4c0/native_handles.metal.hpp"
-#include "m4c0/objc/casts.hpp"
+#include "m4c0/objc/ca_metal_layer.hpp"
+#include "m4c0/objc/ns_window.hpp"
 #include "m4c0/osx/main.hpp"
-
-#include <CoreGraphics/CoreGraphics.h>
 
 int main(int argc, char ** argv) {
   static class : public m4c0::osx::delegate, public m4c0::native_handles, public native_stuff {
     m4c0::fuji::main_loop_thread<loop> m_stuff {};
-    CAMetalLayer * m_layer {};
-    void * m_view {};
-    void * m_window {};
+    m4c0::objc::mtk_view m_view {};
 
   public:
-    void start(void * view) override {
-      m_view = view;
-      m_window = m4c0::objc::objc_msg_send<void *>(view, "window");
-      m_layer = m4c0::objc::objc_msg_send<CAMetalLayer *>(view, "layer");
+    void start(const m4c0::objc::mtk_view * view) override {
+      m_view = *view;
       m_stuff.start(this, this);
     }
     void stop() override {
@@ -25,12 +20,12 @@ int main(int argc, char ** argv) {
     }
 
     [[nodiscard]] CAMetalLayer * layer() const noexcept override {
-      return m_layer;
+      return m_view.layer().self();
     }
 
     void get_mouse_position(float * x, float * y) const override {
-      auto pos = m4c0::objc::objc_msg_send<CGPoint>(m_window, "mouseLocationOutsideOfEventStream");
-      auto bounds = m4c0::objc::objc_msg_send<CGRect>(m_view, "bounds");
+      auto pos = m_view.window().mouse_location_outside_of_event_stream();
+      auto bounds = m_view.bounds();
       *x = pos.x / bounds.size.width;
       *y = (bounds.size.height - pos.y) / bounds.size.height;
     }
