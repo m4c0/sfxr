98c6b4cb60f156398422d72ca1daef601072334f
Eduardo Costa <m4c0@users.noreply.github.com>
2021-05-23 15:25:03 +0100

    events for mouse position on osx

--- src/m4c0.osx.cpp
+++ src/m4c0.osx.cpp
@@ -3,12 +3,17 @@
 #include "m4c0/native_handles.metal.hpp"
 #include "m4c0/objc/ca_metal_layer.hpp"
 #include "m4c0/objc/ns_event.hpp"
+#include "m4c0/objc/ns_event_type.hpp"
 #include "m4c0/objc/ns_window.hpp"
 #include "m4c0/osx/main.hpp"
+
+#include <atomic>
 
 class resources : public m4c0::native_handles, public native_stuff {
   m4c0::fuji::main_loop_thread<loop> m_stuff {};
   m4c0::objc::mtk_view m_view;
+  std::atomic<float> m_mouse_x {};
+  std::atomic<float> m_mouse_y {};
 
 public:
   explicit resources(const m4c0::objc::mtk_view * v) : m_view(*v) {
@@ -28,10 +33,13 @@ public:
   }
 
   void get_mouse_position(float * x, float * y) const override {
-    auto pos = m_view.window().mouse_location_outside_of_event_stream();
+    *x = m_mouse_x;
+    *y = m_mouse_y;
+  }
+  void update_mouse(m4c0::objc::cg_point pos) {
     auto bounds = m_view.bounds();
-    *x = pos.x / bounds.size.width;
-    *y = (bounds.size.height - pos.y) / bounds.size.height;
+    m_mouse_x = static_cast<float>(pos.x / bounds.size.width);
+    m_mouse_y = static_cast<float>((bounds.size.height - pos.y) / bounds.size.height);
   }
 };
 
@@ -44,6 +52,9 @@ int main(int argc, char ** argv) {
       r = std::make_unique<resources>(view);
     }
     void on_event(const m4c0::objc::ns_event * e) override {
+      if (e->type() == m4c0::objc::ns_event_type::mouse_moved) {
+        r->update_mouse(e->location_in_window());
+      }
     }
     void stop() override {
       r.reset();
